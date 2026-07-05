45539adb9f547154ff461a513d64876e4b34e30c
vanshsingone <vanshsingone@gmail.com>
2026-07-05 18:16:57 +0530

    implement AI interview report generation and resume PDF conversion services with Express server initialization

--- Backend/server.js
+++ Backend/server.js
@@ -7,7 +7,8 @@ const connectTODB = require("./src/config/database")
 
 connectTODB()
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000")
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
 })
 
